4d532d56d452512badd62bdf359da5004b7bd6ee
brookecart <brooke.bahn@gmail.com>
2021-01-16 14:36:28 -0800

    added DisplayChart.js to render chart on button click

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Header from './components/Header';
 import TextDisplay from './components/TextDisplay';
+import Chart from './components/Chart';
+import DisplayChart from './components/DisplayChart';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <Header/>
       <TextDisplay/>
+      <DisplayChart/>
     </div>
   );
 }
